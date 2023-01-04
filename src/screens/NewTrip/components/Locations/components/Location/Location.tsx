5ebfe71e35eba47f8c26320cdb407f15ed3fda20
GGO-web <iicandyoii@gmail.com>
2023-01-04 14:56:56 +0200

    Added trip location image carousel

--- src/screens/NewTrip/components/Locations/components/Location/Location.tsx
+++ src/screens/NewTrip/components/Locations/components/Location/Location.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import {
-  View, Text, Image, Button, Colors,
+  View, Text, Image, Button, Colors, Carousel,
 } from 'react-native-ui-lib';
 
 import { useNavigation, useRoute } from '@react-navigation/native';
 
 import { CrumbsLink } from '../../../../../../components/common/CrumbsLink';
 
-import { globalStyles } from '../../../../../../styles/global';
-import { ILocation } from '../../../../../../models/Locations.model';
+import { ILocation, ILocationImage } from '../../../../../../models/Locations.model';
 import { useActions } from '../../../../../../hooks/actions';
+
+import { globalStyles } from '../../../../../../styles/global';
 
 export function Location() {
   const route = useRoute();
@@ -17,7 +18,7 @@ export function Location() {
 
   const { location, locationImage } = route.params as {
     location: ILocation,
-    locationImage: string
+    locationImage: ILocationImage
   };
 
   const { addLocation } = useActions();
@@ -38,23 +39,52 @@ export function Location() {
 
   return (
     <View style={{
-      ...globalStyles.container, ...globalStyles.navcontainer, paddingHorizontal: 0, position: 'relative',
+      ...globalStyles.container,
+      ...globalStyles.navcontainer,
+      paddingHorizontal: 0,
+      position: 'relative',
     }}
     >
-      <Image
-        overlayType={Image.overlayTypes.SOLID}
-        style={{
+      <Carousel
+        animated
+        loop
+        autoplay
+        containerStyle={{
           position: 'absolute',
+          flex: 1,
           top: 0,
           left: 0,
           right: 0,
           bottom: 0,
           zIndex: -2,
+          backgroundColor: Colors.dark,
         }}
-        source={{ uri: locationImage }}
-      />
+      >
+        {location.images.map((tripImage, index) => (
+          <Image
+            // eslint-disable-next-line react/no-array-index-key
+            key={index}
+            overlayType={Image.overlayTypes.SOLID}
+            style={{
+              width: '100%',
+              height: '100%',
+            }}
+            source={{ uri: tripImage?.url || locationImage.url }}
+          />
+        ))}
+      </Carousel>
 
-      <View bottom paddingB-32 flex spread paddingH-20 style={{ zIndex: 2 }}>
+      <View
+        bottom
+        paddingB-32
+        flex
+        spread
+        paddingH-20
+        style={{
+          zIndex: 2,
+          position: 'relative',
+        }}
+      >
         <CrumbsLink iconStyles={{ tintColor: '#fff' }} style={{ marginBottom: 'auto', paddingHorizontal: 0 }}>
           <Text white>Back to locations list</Text>
         </CrumbsLink>
