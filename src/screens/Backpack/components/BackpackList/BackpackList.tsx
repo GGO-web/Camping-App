029f9b81d304be2d819f5f642b20b8df0ddca5b7
GGO-web <iicandyoii@gmail.com>
2023-01-22 15:21:33 +0200

    Added user feedback messages on backpack state

--- src/screens/Backpack/components/BackpackList/BackpackList.tsx
+++ src/screens/Backpack/components/BackpackList/BackpackList.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
 
+import { Text } from 'react-native-ui-lib';
 import { BackpackListItem } from '../BackpackListItem/BackpackListItem';
 
 import { useGetBagItemsQuery } from '../../../../redux/api/trip';
+import { Loader } from '../../../../components/Loader/Loader';
 
 export function BackpackList() {
   const { data: bagItems } = useGetBagItemsQuery();
 
+  if (bagItems === undefined) {
+    return (
+      <Loader
+        message="Items is fetching from the server, please wait a second..."
+      />
+    );
+  }
+
   return (
     <ScrollView>
-      {bagItems?.map((bagItem) => (
+      {bagItems.length ? bagItems?.map((bagItem) => (
         <BackpackListItem key={bagItem.id} backpackItem={bagItem} />
-      ))}
+      )) : (
+        <Text text40 marginT-20 center>
+          There is no items in your backpack, please add some one
+        </Text>
+      )}
     </ScrollView>
   );
 }
