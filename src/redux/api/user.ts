9753cfe2f736f9951648c23d6a53b3ee338c0785
GGO-web <iicandyoii@gmail.com>
2023-01-23 10:51:30 +0200

    Added user api endpoints

--- src/redux/api/user.ts
+++ src/redux/api/user.ts
@@ -1,22 +1,49 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { firebaseAuth } from '../../firebase/firebase';
+import { IMessageResponse } from '../../models/responses/MessageResponse';
 
 import { IUser } from '../../models/User.model';
 
 export const userApi = createApi({
   reducerPath: 'userApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: '',
+    baseUrl: `${process.env.REACT_APP_BACKEND_URL}/user`,
   }),
   tagTypes: ['User'],
   endpoints: (builder) => ({
-    getUserById: builder.query<IUser, string>({
-      query: (userId) => ({
-        url: `${process.env.REACT_APP_BACKEND_URL}/user/${userId}`,
-        method: 'get',
+    getUser: builder.query<IUser, void>({
+      query: () => ({
+        url: `/${firebaseAuth.currentUser?.uid}`,
       }),
       providesTags: ['User'],
+    }),
+    updateUserAvatar: builder.mutation<IMessageResponse, string>({
+      query: (avatar) => ({
+        url: '/avatar',
+        body: {
+          uid: firebaseAuth.currentUser?.uid,
+          avatar,
+        },
+        method: 'PATCH',
+      }),
+      invalidatesTags: ['User'],
+    }),
+    updateUserProfile: builder.mutation<IMessageResponse, IUser>({
+      query: (user) => ({
+        url: '/',
+        body: {
+          ...user,
+          uid: firebaseAuth.currentUser?.uid,
+        },
+        method: 'PATCH',
+      }),
+      invalidatesTags: ['User'],
     }),
   }),
 });
 
-export const { useLazyGetUserByIdQuery } = userApi;
+export const {
+  useGetUserQuery,
+  useUpdateUserAvatarMutation,
+  useUpdateUserProfileMutation,
+} = userApi;
